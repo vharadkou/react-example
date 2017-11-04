09b6fcf990e1d0313af11fe5cb03a8eae3c4d7ba
Valiantsin Haradkou <valicser@yandex.ru>
2017-11-05 02:02:04 +0300

    api

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react';
 
+import * as axios from 'axios';
+
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, undefined> {
-    render() {
+
+    public componentWillMount(): void {
+        let headers = new Headers({
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Basic a2V5X01qZzNNRGc1TVRBNU5qVTROV0k1TVRBMU9HTTJZamhtTmpOak1XSTI6'
+        });
+
+        fetch('https://api.threadgenius.co/v1/catalog', {
+            method: 'GET',
+            mode: 'no-cors',
+            headers
+        }).then(data => {
+            console.log(data);
+        });
+
+    }
+
+
+    public render(): JSX.Element {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
